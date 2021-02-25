f6f2aec0fc701e24485e1696d4e9d0e540bdd2af
enhod <79565666+enhod@users.noreply.github.com>
2021-02-24 21:45:02 -0800

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+	int x = 20;
+	int y = 5;
+	int z = 50;
+	
+	
+	cout<<"x+y=";
+	cout<<x+y<<endl;
+	cout<<"z-x=";
+	cout<<z-x<<endl;
+	cout<<"x*y=";
+	cout<<x*y<<endl;
+	cout<<"z/y=";
+	cout<<z/y;
+	
+	
+	return 0;
+}
